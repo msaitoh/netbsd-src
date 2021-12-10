ac9b58bdb9e4c05c5e87804afb78041788a4d6dd
Masanobu SAITOH <msaitoh@execsw.org>
2021-12-10 15:48:44 +0900

    Change the first argument of ixgbe_send_vf_msg(). No functional change.
     - This is a part of FreeBSD ix-3.2.17.

--- sys/dev/pci/ixgbe/if_sriov.c
+++ sys/dev/pci/ixgbe/if_sriov.c
@@ -94,26 +94,26 @@ ixgbe_align_all_queue_indices(struct adapter *adapter)
 
 /* Support functions for SR-IOV/VF management */
 static inline void
-ixgbe_send_vf_msg(struct adapter *adapter, struct ixgbe_vf *vf, u32 msg)
+ixgbe_send_vf_msg(struct ixgbe_hw *hw, struct ixgbe_vf *vf, u32 msg)
 {
 	if (vf->flags & IXGBE_VF_CTS)
 		msg |= IXGBE_VT_MSGTYPE_CTS;
 
-	adapter->hw.mbx.ops.write(&adapter->hw, &msg, 1, vf->pool);
+	hw->mbx.ops.write(hw, &msg, 1, vf->pool);
 }
 
 static inline void
 ixgbe_send_vf_ack(struct adapter *adapter, struct ixgbe_vf *vf, u32 msg)
 {
 	msg &= IXGBE_VT_MSG_MASK;
-	ixgbe_send_vf_msg(adapter, vf, msg | IXGBE_VT_MSGTYPE_SUCCESS);
+	ixgbe_send_vf_msg(&adapter->hw, vf, msg | IXGBE_VT_MSGTYPE_SUCCESS);
 }
 
 static inline void
 ixgbe_send_vf_nack(struct adapter *adapter, struct ixgbe_vf *vf, u32 msg)
 {
 	msg &= IXGBE_VT_MSG_MASK;
-	ixgbe_send_vf_msg(adapter, vf, msg | IXGBE_VT_MSGTYPE_FAILURE);
+	ixgbe_send_vf_msg(&adapter->hw, vf, msg | IXGBE_VT_MSGTYPE_FAILURE);
 }
 
 static inline void
@@ -209,7 +209,7 @@ ixgbe_ping_all_vfs(struct adapter *adapter)
 	for (int i = 0; i < adapter->num_vfs; i++) {
 		vf = &adapter->vfs[i];
 		if (vf->flags & IXGBE_VF_ACTIVE)
-			ixgbe_send_vf_msg(adapter, vf, IXGBE_PF_CONTROL_MSG);
+			ixgbe_send_vf_msg(&adapter->hw, vf, IXGBE_PF_CONTROL_MSG);
 	}
 } /* ixgbe_ping_all_vfs */
 
@@ -797,7 +797,7 @@ ixgbe_init_vf(struct adapter *adapter, struct ixgbe_vf *vf)
 	ixgbe_vf_enable_transmit(adapter, vf);
 	ixgbe_vf_enable_receive(adapter, vf);
 
-	ixgbe_send_vf_msg(adapter, vf, IXGBE_PF_CONTROL_MSG);
+	ixgbe_send_vf_msg(&adapter->hw, vf, IXGBE_PF_CONTROL_MSG);
 } /* ixgbe_init_vf */
 
 void
